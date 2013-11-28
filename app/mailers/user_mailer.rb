fefc980544d05b0b4d09bab33c906b772ce6f0ea
Paul Blizzard <paul@bibliocommons.com>
2013-11-28 15:09:03 -0500

    .

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,16 +10,16 @@ class UserMailer < ActionMailer::Base
   def signup_confirmation(person)
     @person = person
     mail (:to       =>    @person.email, 
-          :subject  =>    "Confirmation - Queens Park Auction", 
-          :reply_to =>    "pressgalleryauction@gmail.com")
+          :subject  =>    'Confirmation - Queens Park Auction', 
+          :reply_to =>    'pressgalleryauction@gmail.com')
   end
 
   def returning_user_confirmation(person)
     @person = person
     
     mail (:to       =>    @person.email, 
-          :subject  =>    "Queens Park Auction - Welcome Back!", 
-          :reply_to =>    "pressgalleryauction@gmail.com")
+          :subject  =>    'Queens Park Auction - Welcome Back!', 
+          :reply_to =>    'pressgalleryauction@gmail.com')
     
     end
   end
