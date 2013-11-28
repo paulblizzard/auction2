1163bf97a1f2144873d4ec1179bec156f29196f9
root <root@domU-12-31-39-14-21-65.compute-1.internal>
2013-11-28 01:14:04 +0000

    initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Auction2::Application.config.session_store :cookie_store, key: '_auction2_session'
+Auction2::Application.config.session_store :cookie_store, :key => '_auction2_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
