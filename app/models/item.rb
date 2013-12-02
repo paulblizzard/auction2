8733f220543cf5612fafe4e154cf34fb608cd971
Paul Blizzard <paul@bibliocommons.com>
2013-12-02 17:23:18 -0500

    reverting mende's commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ActiveRecord::Base
   attr_accessible :description, :donor_id, :minimum_bid, :name, :winning_bid, :winning_bidder_id
   validates_presence_of :name, :on => :create, :message => "can't be blank"
   validates_presence_of :minimum_bid, :on => :create, :message => "can't be blank"
+  validates_presence_of :donor_id, :on => :create, :message => "can't be blank"
 end
